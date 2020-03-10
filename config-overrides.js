15cd660648cfa65c6b19f0d76dcc795328229d91
lilingjie <979520102@qq.com>
2020-03-10 12:59:31 +0800

    add antd

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+// 配置用于按需加载组件代码和样式
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: { '@primary-color': '#1DA57A' },
+    }),
+);
+
